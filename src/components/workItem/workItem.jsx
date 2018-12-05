2499ffae1b49b8fc8042d67f23a271bb27e7d529
lihaiyang <18181378011@163.com>
2018-12-05 17:17:13 +0800

    进一步完善与修改

--- src/components/workItem/workItem.jsx
+++ src/components/workItem/workItem.jsx
@@ -1,12 +1,11 @@
 import React,{Component} from 'react'
 import PropTypes from 'prop-types'
-import { Checkbox,Button} from 'antd';
+import { Checkbox,Button,Modal} from 'antd';
 export default class WorkList extends Component{
     style = {
-        float:'right',
         position:'absolute',
         right:'10px' ,
-        top:'8px'
+        top:'12px'
     }
     static propTypes = {
         work:PropTypes.object.isRequired,
@@ -15,10 +14,17 @@ export default class WorkList extends Component{
         del:PropTypes.func.isRequired,
         update:PropTypes.func.isRequired
     }
-
+    state = {
+        isDel:false
+    }
     delWork = () => {
         const {index} = this.props
-        this.props.del(index)
+        debugger
+         this.showConfirm()
+        if( this.state.isDel){
+                this.props.del(index)
+        }
+
     }
 
     updateWork = () =>{
@@ -26,12 +32,35 @@ export default class WorkList extends Component{
         this.props.update(index)
     }
 
+
+    //提示警告
+    showConfirm = () => {
+        const confirm  = Modal.confirm;
+        const that = this
+        confirm({
+            title: `确定删除“${this.props.work.workName}”任务吗？`,
+            onOk() {
+              that.setState({
+                  isDel:true
+              })
+            },
+            onCancel() {
+                that.setState({
+                    isDel:false
+                })
+            },
+        });
+    }
     render(){
         const {work,isFinished} = this.props
         return (
             <div>
-                <li className="list-group-item" >
-                    <span>{work.workName}</span>
+                <li className="list-group-item " style={{paddingBottom:'25px'}} >
+                    <div style={{position:'relative'}}>
+                        <span >{work.workName}</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                        <span style={{position:'absolute',left:'30px',top:'20px',fontSize:'12px'}}>完成时间<span style={{color:'blue'}}>{work.date}</span></span>
+                    </div>
+
                     <div style={this.style}>
                         {isFinished ? <Checkbox checked={this.props.work.checked} onChange={this.updateWork}>标记为未完成</Checkbox> :
                             <Checkbox checked={this.props.work.checked } onChange={this.updateWork}>标记为已完成</Checkbox>}
