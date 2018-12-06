ed23383fbebfcf3f11911a024189aaf6c6dffa08
lihaiyang <18181378011@163.com>
2018-12-06 17:26:25 +0800

    新增redux版本的待办清单

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,54 +3,9 @@ import {connect} from 'react-redux'
 import Completed from '../completed/completed'
 import Unfinished from '../unfinished/unfinished'
 import WorkAdd from '../workAdd/work-add'
-import {Divider ,Modal} from 'antd'
+import {Divider } from 'antd'
  class App extends Component{
-
-    /*/!*state = {
-        works:[
-            {workName:'英语听力',isfinished:true,checked:true,date:'2018-04-15'},
-            {workName:'打篮球',isfinished:false,checked:false,date:'2018-04-05'},
-            {workName:'游泳',isfinished:false,checked:false,date:'2018-06-15'},
-            {workName:'蹦极',isfinished:true,checked:true,date:'2018-08-15'},
-        ],
-        isShow:false
-    }*!/
-
-    warning = () => {
-        Modal.warning({
-            title: '任务名不能为空！',
-            // content: 'some messages...some messages...',
-        });
-    }
-
-    //往works里面添加数据
-    add = (workName,date) => {
-        const {works} = this.props.project
-        if(!workName){
-            this.warning()
-            return
-        }
-        works.unshift({workName,isfinished:false,date})
-        this.setState({works})
-    }
-
-    //删除works中指定数据
-    del = (index) => {
-        const {works} = this.props.project
-        works.splice(index,1)
-        this.setState({works})
-    }
-
-    //更新work状态
-    update = (index) => {
-        const {works} = this.props.project
-        works[index].isfinished =  !works[index].isfinished
-        works[index].checked =  !works[index].checked
-        this.setState({works})
-    }*/
-
     render(){
-        const {works} = this.props.project
         return(
 
             <div>
